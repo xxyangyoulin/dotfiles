e0d922bbb64974209566bb4fa6854549bb548f3e
yyl <xxyangyoulin@gmail.com>
2025-11-09 13:47:59 +0800

    Sync

---
+++ scripts/sh/clock_week.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ~/.local/bin/waybar-cn-clock
+# 输出格式：月/日 周一 12:31:12
+
+month=$(date +%m)
+day=$(date +%d)
+time=$(date +%H:%M:%S)
+w=$(date +%u)  # 1..7
+
+map=(一 二 三 四 五 六 日)
+
+echo "周${map[$((w-1))]}"
+
